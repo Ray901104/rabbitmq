d2899be8f7f05155e4d75a3f46370e74bfcd9c73
ip323 <ip323@naver.com>
2025-02-16 14:29:37 +0900

    step3 주석처리

--- HelloMessageQueue/src/main/java/com/hello/hellomessagequeue/step3/NotificationController.java
+++ HelloMessageQueue/src/main/java/com/hello/hellomessagequeue/step3/NotificationController.java
@@ -1,23 +1,23 @@
-package com.hello.hellomessagequeue.step3;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/notifications")
-public class NotificationController {
-
-    private final NotificationPublisher notificationPublisher;
-
-    public NotificationController(NotificationPublisher notificationPublisher) {
-        this.notificationPublisher = notificationPublisher;
-    }
-
-    @PostMapping
-    public String sendNotification(@RequestBody String message) {
-        notificationPublisher.publish(message);
-        return "[#] Notification Sent: " + message + "\n";
-    }
-}
+//package com.hello.hellomessagequeue.step3;
+//
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/notifications")
+//public class NotificationController {
+//
+//    private final NotificationPublisher notificationPublisher;
+//
+//    public NotificationController(NotificationPublisher notificationPublisher) {
+//        this.notificationPublisher = notificationPublisher;
+//    }
+//
+//    @PostMapping
+//    public String sendNotification(@RequestBody String message) {
+//        notificationPublisher.publish(message);
+//        return "[#] Notification Sent: " + message + "\n";
+//    }
+//}
