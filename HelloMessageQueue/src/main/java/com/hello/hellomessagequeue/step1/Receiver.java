c1a8848d7b235f55bd466e60845dcfac2192903b
ip323 <ip323@naver.com>
2025-02-16 12:43:58 +0900

    step1, step2 주석처리

--- HelloMessageQueue/src/main/java/com/hello/hellomessagequeue/step1/Receiver.java
+++ HelloMessageQueue/src/main/java/com/hello/hellomessagequeue/step1/Receiver.java
@@ -1,11 +1,11 @@
-package com.hello.hellomessagequeue.step1;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class Receiver {
-
-    public void receiveMessage(String message) {
-        System.out.println("[#] Received : " + message);
-    }
-}
+//package com.hello.hellomessagequeue.step1;
+//
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class Receiver {
+//
+//    public void receiveMessage(String message) {
+//        System.out.println("[#] Received : " + message);
+//    }
+//}
--- HelloMessageQueue/src/main/java/com/hello/hellomessagequeue/step2/WorkQueueController.java
+++ HelloMessageQueue/src/main/java/com/hello/hellomessagequeue/step2/WorkQueueController.java
@@ -1,23 +1,23 @@
-package com.hello.hellomessagequeue.step2;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api/v1")
-public class WorkQueueController {
-
-    private final WorkQueueProducer workQueueProducer;
-
-    public WorkQueueController(WorkQueueProducer workQueueProducer) {
-        this.workQueueProducer = workQueueProducer;
-    }
-
-    @PostMapping("/workqueue")
-    public String workQueue(@RequestParam String message, @RequestParam int duration) {
-        workQueueProducer.sendWorkQueue(message, duration);
-        return "# WorkQueue sent successfully! " + message + " | " + duration;
-    }
-}
+//package com.hello.hellomessagequeue.step2;
+//
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/api/v1")
+//public class WorkQueueController {
+//
+//    private final WorkQueueProducer workQueueProducer;
+//
+//    public WorkQueueController(WorkQueueProducer workQueueProducer) {
+//        this.workQueueProducer = workQueueProducer;
+//    }
+//
+//    @PostMapping("/workqueue")
+//    public String workQueue(@RequestParam String message, @RequestParam int duration) {
+//        workQueueProducer.sendWorkQueue(message, duration);
+//        return "# WorkQueue sent successfully! " + message + " | " + duration;
+//    }
+//}
