cca7c1fecf542d4fe9ad32dfbd2f1ba05d9149e9
ip323 <ip323@naver.com>
2025-02-17 20:21:05 +0900

    step4 주석처리

--- HelloMessageQueue/src/main/java/com/hello/hellomessagequeue/step4/NewsController.java
+++ HelloMessageQueue/src/main/java/com/hello/hellomessagequeue/step4/NewsController.java
@@ -1,23 +1,23 @@
-package com.hello.hellomessagequeue.step4;
-
-import org.springframework.messaging.handler.annotation.Header;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class NewsController {
-
-    private final NewsPublisher newsPublisher;
-
-    public NewsController(NewsPublisher newsPublisher) {
-        this.newsPublisher = newsPublisher;
-    }
-
-    @MessageMapping("/subscribe")
-    public void handleSubscription(@Header("newsType") String newsType) {
-        System.out.println("[#] newsType = " + newsType);
-
-        String newsMessage = newsPublisher.publish(newsType);
-        System.out.println("[#] newsMessage = " + newsMessage);
-    }
-}
+//package com.hello.hellomessagequeue.step4;
+//
+//import org.springframework.messaging.handler.annotation.Header;
+//import org.springframework.messaging.handler.annotation.MessageMapping;
+//import org.springframework.stereotype.Controller;
+//
+//@Controller
+//public class NewsController {
+//
+//    private final NewsPublisher newsPublisher;
+//
+//    public NewsController(NewsPublisher newsPublisher) {
+//        this.newsPublisher = newsPublisher;
+//    }
+//
+//    @MessageMapping("/subscribe")
+//    public void handleSubscription(@Header("newsType") String newsType) {
+//        System.out.println("[#] newsType = " + newsType);
+//
+//        String newsMessage = newsPublisher.publish(newsType);
+//        System.out.println("[#] newsMessage = " + newsMessage);
+//    }
+//}
--- HelloMessageQueue/src/main/java/com/hello/hellomessagequeue/step4/WebSocketConfig.java
+++ HelloMessageQueue/src/main/java/com/hello/hellomessagequeue/step4/WebSocketConfig.java
@@ -1,21 +1,21 @@
-package com.hello.hellomessagequeue.step4;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-
-@Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
-    @Override
-    public void configureMessageBroker(org.springframework.messaging.simp.config.MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic");
-        registry.setApplicationDestinationPrefixes("/app");
-    }
-
-    @Override
-    public void registerStompEndpoints(org.springframework.web.socket.config.annotation.StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
-    }
-}
+//package com.hello.hellomessagequeue.step4;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+//import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+//
+//@Configuration
+//@EnableWebSocketMessageBroker
+//public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+//
+//    @Override
+//    public void configureMessageBroker(org.springframework.messaging.simp.config.MessageBrokerRegistry registry) {
+//        registry.enableSimpleBroker("/topic");
+//        registry.setApplicationDestinationPrefixes("/app");
+//    }
+//
+//    @Override
+//    public void registerStompEndpoints(org.springframework.web.socket.config.annotation.StompEndpointRegistry registry) {
+//        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+//    }
+//}
