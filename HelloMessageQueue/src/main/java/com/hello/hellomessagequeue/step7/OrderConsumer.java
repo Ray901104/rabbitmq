742bc80378dd2d85768989e60189531e0afd8484
ip323 <ip323@naver.com>
2025-02-18 10:16:45 +0900

    step7 주석처리

--- HelloMessageQueue/src/main/java/com/hello/hellomessagequeue/step7/OrderConsumer.java
+++ HelloMessageQueue/src/main/java/com/hello/hellomessagequeue/step7/OrderConsumer.java
@@ -1,45 +1,45 @@
-package com.hello.hellomessagequeue.step7;
-
-import com.rabbitmq.client.Channel;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.messaging.handler.annotation.Header;
-import org.springframework.stereotype.Component;
-
-import java.io.IOException;
-
-@Component
-public class OrderConsumer {
-
-    private static final int MAX_RETRIES = 3; // 총 시도 제한 수
-    private int retryCount = 0; // 재시도 횟수
-
-    @RabbitListener(queues = RabbitMQConfig.ORDER_COMPLETED_QUEUE, containerFactory = "rabbitListenerContainerFactory")
-    public void processOrder(String message, Channel channel, @Header("amqp_deliveryTag") long tag) {
-        try {
-            // 실패 유발
-            if ("fail".equalsIgnoreCase(message)) {
-                if (retryCount < MAX_RETRIES) {
-                    System.err.println("[Consumer] 실패, 재시도 = " + retryCount);
-                    retryCount++;
-                    throw new RuntimeException(message);
-                } else {
-                    System.err.println("[Consumer] 최대 재시도 횟수 초과, DLQ 이동");
-                    retryCount = 0;
-                    channel.basicNack(tag, false, false);
-                    return;
-                }
-            }
-            System.out.println("[Consumer] 성공 : " + message);
-            channel.basicAck(tag, false);
-            retryCount = 0;
-        } catch (Exception e) {
-            System.err.println("[Consumer] 예외 발생 : " + e.getMessage());
-            try {
-                // 실패 시 basicReject 처리하여 메시지를 다시 처리
-                channel.basicReject(tag, true);
-            } catch (IOException ex) {
-                System.err.println("[Consumer] fail & reject message : " + ex.getMessage());
-            }
-        }
-    }
-}
+//package com.hello.hellomessagequeue.step7;
+//
+//import com.rabbitmq.client.Channel;
+//import org.springframework.amqp.rabbit.annotation.RabbitListener;
+//import org.springframework.messaging.handler.annotation.Header;
+//import org.springframework.stereotype.Component;
+//
+//import java.io.IOException;
+//
+//@Component
+//public class OrderConsumer {
+//
+//    private static final int MAX_RETRIES = 3; // 총 시도 제한 수
+//    private int retryCount = 0; // 재시도 횟수
+//
+//    @RabbitListener(queues = RabbitMQConfig.ORDER_COMPLETED_QUEUE, containerFactory = "rabbitListenerContainerFactory")
+//    public void processOrder(String message, Channel channel, @Header("amqp_deliveryTag") long tag) {
+//        try {
+//            // 실패 유발
+//            if ("fail".equalsIgnoreCase(message)) {
+//                if (retryCount < MAX_RETRIES) {
+//                    System.err.println("[Consumer] 실패, 재시도 = " + retryCount);
+//                    retryCount++;
+//                    throw new RuntimeException(message);
+//                } else {
+//                    System.err.println("[Consumer] 최대 재시도 횟수 초과, DLQ 이동");
+//                    retryCount = 0;
+//                    channel.basicNack(tag, false, false);
+//                    return;
+//                }
+//            }
+//            System.out.println("[Consumer] 성공 : " + message);
+//            channel.basicAck(tag, false);
+//            retryCount = 0;
+//        } catch (Exception e) {
+//            System.err.println("[Consumer] 예외 발생 : " + e.getMessage());
+//            try {
+//                // 실패 시 basicReject 처리하여 메시지를 다시 처리
+//                channel.basicReject(tag, true);
+//            } catch (IOException ex) {
+//                System.err.println("[Consumer] fail & reject message : " + ex.getMessage());
+//            }
+//        }
+//    }
+//}
--- HelloMessageQueue/src/main/java/com/hello/hellomessagequeue/step7/OrderController.java
+++ HelloMessageQueue/src/main/java/com/hello/hellomessagequeue/step7/OrderController.java
@@ -1,24 +1,24 @@
-package com.hello.hellomessagequeue.step7;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-@RequestMapping("/api/order")
-@RestController
-public class OrderController {
-
-    private final OrderProducer orderProducer;
-
-    public OrderController(OrderProducer orderProducer) {
-        this.orderProducer = orderProducer;
-    }
-
-    @GetMapping
-    public ResponseEntity<String> sendOrderMessage(@RequestParam("message") String message) {
-        orderProducer.sendShipping(message);
-        return ResponseEntity.ok("[Controller] Order Completed Message sent : " + message);
-    }
-}
+//package com.hello.hellomessagequeue.step7;
+//
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RequestMapping("/api/order")
+//@RestController
+//public class OrderController {
+//
+//    private final OrderProducer orderProducer;
+//
+//    public OrderController(OrderProducer orderProducer) {
+//        this.orderProducer = orderProducer;
+//    }
+//
+//    @GetMapping
+//    public ResponseEntity<String> sendOrderMessage(@RequestParam("message") String message) {
+//        orderProducer.sendShipping(message);
+//        return ResponseEntity.ok("[Controller] Order Completed Message sent : " + message);
+//    }
+//}
