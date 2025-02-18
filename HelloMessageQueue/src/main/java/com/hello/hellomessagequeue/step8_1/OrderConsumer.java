27c6338546029dd07b014c0ebb2f2e1ce20f802e
ip323 <ip323@naver.com>
2025-02-18 10:52:54 +0900

    step8-1 주석 처리

--- HelloMessageQueue/src/main/java/com/hello/hellomessagequeue/step8_1/OrderConsumer.java
+++ HelloMessageQueue/src/main/java/com/hello/hellomessagequeue/step8_1/OrderConsumer.java
@@ -1,39 +1,39 @@
-package com.hello.hellomessagequeue.step8_1;
-
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.retry.support.RetryTemplate;
-import org.springframework.stereotype.Component;
-
-@Component
-public class OrderConsumer {
-
-    private final RabbitTemplate rabbitTemplate;
-    private final RetryTemplate retryTemplate;
-
-    public OrderConsumer(RabbitTemplate rabbitTemplate, RetryTemplate retryTemplate) {
-        this.rabbitTemplate = rabbitTemplate;
-        this.retryTemplate = retryTemplate;
-    }
-
-    @RabbitListener(queues = RabbitMQConfig.ORDER_COMPLETED_QUEUE)
-    public void consume(String message) {
-        retryTemplate.execute(context -> {
-            try {
-                System.out.println("[Consumer] Received : " + message + " # retry : " + context.getRetryCount());
-
-                if ("fail".equalsIgnoreCase(message)) {
-                    throw new RuntimeException(message);
-                }
-                System.out.println("[Consumer] Success : " + message);
-            } catch (Exception e) {
-                if (context.getRetryCount() >= 2) {
-                    rabbitTemplate.convertAndSend(RabbitMQConfig.DLX, RabbitMQConfig.DEAD_LETTER_ROUTING_KEY, message);
-                } else {
-                    throw e;
-                }
-            }
-            return null;
-        });
-    }
-}
+//package com.hello.hellomessagequeue.step8_1;
+//
+//import org.springframework.amqp.rabbit.annotation.RabbitListener;
+//import org.springframework.amqp.rabbit.core.RabbitTemplate;
+//import org.springframework.retry.support.RetryTemplate;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class OrderConsumer {
+//
+//    private final RabbitTemplate rabbitTemplate;
+//    private final RetryTemplate retryTemplate;
+//
+//    public OrderConsumer(RabbitTemplate rabbitTemplate, RetryTemplate retryTemplate) {
+//        this.rabbitTemplate = rabbitTemplate;
+//        this.retryTemplate = retryTemplate;
+//    }
+//
+//    @RabbitListener(queues = RabbitMQConfig.ORDER_COMPLETED_QUEUE)
+//    public void consume(String message) {
+//        retryTemplate.execute(context -> {
+//            try {
+//                System.out.println("[Consumer] Received : " + message + " # retry : " + context.getRetryCount());
+//
+//                if ("fail".equalsIgnoreCase(message)) {
+//                    throw new RuntimeException(message);
+//                }
+//                System.out.println("[Consumer] Success : " + message);
+//            } catch (Exception e) {
+//                if (context.getRetryCount() >= 2) {
+//                    rabbitTemplate.convertAndSend(RabbitMQConfig.DLX, RabbitMQConfig.DEAD_LETTER_ROUTING_KEY, message);
+//                } else {
+//                    throw e;
+//                }
+//            }
+//            return null;
+//        });
+//    }
+//}
--- HelloMessageQueue/src/main/java/com/hello/hellomessagequeue/step8_1/RetryConfig.java
+++ HelloMessageQueue/src/main/java/com/hello/hellomessagequeue/step8_1/RetryConfig.java
@@ -1,29 +1,29 @@
-package com.hello.hellomessagequeue.step8_1;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.retry.backoff.FixedBackOffPolicy;
-import org.springframework.retry.policy.SimpleRetryPolicy;
-import org.springframework.retry.support.RetryTemplate;
-
-@Configuration
-public class RetryConfig {
-
-    @Bean
-    public RetryTemplate retryTemplate() {
-        RetryTemplate retryTemplate = new RetryTemplate();
-
-        // 재시도 정책 설정 : 최대 3번 시도
-        SimpleRetryPolicy simpleRetryPolicy = new SimpleRetryPolicy();
-        simpleRetryPolicy.setMaxAttempts(3);
-
-        // 백오프 정책 설정 : 재시도 간격 1초
-        FixedBackOffPolicy fixedBackOffPolicy = new FixedBackOffPolicy();
-        fixedBackOffPolicy.setBackOffPeriod(1000L);
-
-        retryTemplate.setRetryPolicy(simpleRetryPolicy);
-        retryTemplate.setBackOffPolicy(fixedBackOffPolicy);
-
-        return retryTemplate;
-    }
-}
+//package com.hello.hellomessagequeue.step8_1;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.retry.backoff.FixedBackOffPolicy;
+//import org.springframework.retry.policy.SimpleRetryPolicy;
+//import org.springframework.retry.support.RetryTemplate;
+//
+//@Configuration
+//public class RetryConfig {
+//
+//    @Bean
+//    public RetryTemplate retryTemplate() {
+//        RetryTemplate retryTemplate = new RetryTemplate();
+//
+//        // 재시도 정책 설정 : 최대 3번 시도
+//        SimpleRetryPolicy simpleRetryPolicy = new SimpleRetryPolicy();
+//        simpleRetryPolicy.setMaxAttempts(3);
+//
+//        // 백오프 정책 설정 : 재시도 간격 1초
+//        FixedBackOffPolicy fixedBackOffPolicy = new FixedBackOffPolicy();
+//        fixedBackOffPolicy.setBackOffPeriod(1000L);
+//
+//        retryTemplate.setRetryPolicy(simpleRetryPolicy);
+//        retryTemplate.setBackOffPolicy(fixedBackOffPolicy);
+//
+//        return retryTemplate;
+//    }
+//}
